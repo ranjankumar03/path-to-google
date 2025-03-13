9b1de0b941301dac336f291d1481c183ddcec516
Ranjan Kumar <hereisranjan@gmail.com>
2025-03-13 17:12:59 +0800

    formatted

--- 901_Stock_Span.java
+++ 901_Stock_Span.java
@@ -1,6 +1,6 @@
 class StockSpanner {
 
-    public static void main(String [] args){
+    public static void main(String [] args) {
 
         int[] arr = {10, 4, 5, 90, 120, 80};
 
@@ -12,17 +12,17 @@ class StockSpanner {
         
     }
 
-    private static int[] calculateSpanBrute(int[] arr ){
+    private static int[] calculateSpanBrute(int[] arr ) {
         int len = arr.length;
 
         int[] spanner  = new int[len];
 
-        for(int i = 0; i < len; i++){
+        for(int i = 0; i < len; i++) {
             spanner[i] = 1;
         }
 
-        for(int i = 1; i < len; i++){
-            for(int j = i-1; j>=0 && arr[i] >= arr[j]; j--){
+        for(int i = 1; i < len; i++) {
+            for(int j = i-1; j>=0 && arr[i] >= arr[j]; j--) {
                 spanner[i] = spanner[i] + 1;
             }
 
