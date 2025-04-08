0708983cc102d12b07f2962b83d12e4cbe950e6d
Ranjan Kumar <hereisranjan@gmail.com>
2025-04-08 11:11:14 +0800

    max min problem - hk

---
+++ hackerrank/MaxMin.java
@@ -0,0 +1,28 @@
+package hackerrank;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class MaxMin{
+    public static void main(String[] args){
+
+        int[] input = {1,4,7,2};
+        List<Integer> arr = Arrays.stream(input).boxed().collect(Collectors.toList());
+        System.out.println(maxmin(2, arr));
+    }
+
+    private static int maxmin(int k, List<Integer> arr){
+
+        Collections.sort(arr);
+
+        int min = Integer.MAX_VALUE;
+
+        for(int i=0; i<(arr.size() - k + 1 ); i++){
+            min = Math.min(min, arr.get(i + k -1) - arr.get(i));
+        }
+        return min;
+
+    }
+}
